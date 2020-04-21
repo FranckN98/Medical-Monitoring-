5a360a411c99af6a32b037949579a6a11fb5aa38
Franck Ngami <ufrank238@yahoo.fr>
2020-04-21 02:27:36 +0200

    Search Interface

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,8 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage 
+{
+  logInwithId:boolean = true; 
 
-  constructor() {}
+  constructor() 
+  {
+    
+  }
 
 }

5849afa07bca00946e99615570ece0c233007dfb
Franck Ngami <ufrank238@yahoo.fr>
2020-04-25 03:53:22 +0200

    Info interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'menu', pathMatch: 'full' },
+  { path: '', redirectTo: 'info', pathMatch: 'full' },
   { path: 'menu', loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule)},
-  
+  { path: 'info', loadChildren: () => import('./information/information.module').then( m => m.InformationPageModule)},
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
 ];
 

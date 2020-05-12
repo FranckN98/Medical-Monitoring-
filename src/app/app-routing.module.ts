57b3ceebb390529daf8aad24404b1fd69bdd7d0b
ema <ema@netlab2.local>
2020-05-12 04:37:32 +0200

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,15 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'who', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'menu', loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule)},
   { path: 'menu', loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule)},
   { path: 'info', loadChildren: () => import('./information/information.module').then( m => m.InformationPageModule)},
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  {  path: 'history', loadChildren: () => import('./history/history.module').then( m => m.HistoryPageModule)},
+  { path: 'history', loadChildren: () => import('./history/history.module').then( m => m.HistoryPageModule)},
   { path: 'consult', loadChildren: () => import('./consultation/consultation.module').then( m => m.ConsultationPageModule)},
   { path: 'who',loadChildren: () => import('./who-ami/who-ami.module').then( m => m.WhoAmiPageModule)},
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
+  { path: 'login',loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule) },
  
 ];
 
